f0485b43b3ca3c3ae2a160d660a6a69e86bd529a
olys6 <oliver.skjevesland@gmail.com>
2022-03-29 23:57:45 +0200

    added engineer and developer to the loop of logos

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,10 @@ const Home = () => {
 
   const [i, setI] = useState(0)
   const languageIconElemsArray = [
-    <i className="fa-brands fa-js-square"></i>, <i className="fa-brands fa-react"></i>, 
-    <i className="fa-brands fa-python"></i>, <i className="fa-brands fa-swift"></i>, 
-    <i className="fa-brands fa-wordpress"></i>, <i className="fa-brands fa-node-js"></i>,
-    <i className="fa-brands fa-html5"></i>, <i className="fa-brands fa-css3-alt"></i>,
-    <i className="fa-brands fa-bootstrap"></i>,
+    <span><i style={{ color: "#ffdc04" }} className="fa-brands fa-js-square"></i> JavaScript engineer</span>, <span><i style={{ color: "#68dcfc" }} className="fa-brands fa-react"></i> React frontend developer</span>, 
+    <span><i className="fa-brands fa-python"></i> Python engineer</span>, <span><i className="fa-brands fa-swift"></i> Swift engineer</span>, 
+    <span><i className="fa-brands fa-wordpress"> WordPress developer</i> </span>, <span><i className="fa-brands fa-node-js"></i> Node.js developer</span>,
+    <span><i className="fa-brands fa-html5"></i> HTML developer</span>, <span><i className="fa-brands fa-css3-alt"></i> CSS developer</span>,
   ]
 
   useEffect(() => {
@@ -20,14 +19,14 @@ const Home = () => {
       } else if (i < languageIconElemsArray.length) {
         setI(i + 1)
       }
-    }, 500)
+    }, 1000)
   }, [i, languageIconElemsArray.length])
 
   return(
     <section className="faceAndQuoteSection">
       <img className="ollyFacePfp" alt="Oliver's face" src="/assets/OllyPFP.jpg"/>
       <div className="homeNameAndQuote">
-        <Typography color="secondary" variant="h4">Hi my name is Oliver Skjevesland and I'm a <span style={{ fontSize: "40px" }}>{languageIconElemsArray[i]}</span> developer.</Typography>
+        <Typography color="secondary" variant="h4">Hi my name is Oliver Skjevesland and I'm a {languageIconElemsArray[i]}.</Typography>
         <br />
         <Typography color="secondary" variant="h5">"My goal is to improve and learn new skills with IT-based work experience allowing me to develop programs that will be helpful in everyday life."</Typography>
       </div>
