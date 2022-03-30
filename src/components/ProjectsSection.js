5eba95229cc21f5329c3b7ef161c68cd2c6d06f3
olys6 <oliver.skjevesland@gmail.com>
2022-03-30 17:36:47 +0200

    added buttons and link to all of the projects

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Typography, Button } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
@@ -16,25 +16,25 @@ const ProjectsSection = () => {
 
   const handleChange1 = (panel1) => (event, isExpanded1) => {
     setExpanded1(isExpanded1 ? panel1 : false);
-    (isExpanded1 ? setExpandedBrightness1("linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)), url(/assets/PCEnthusiasts/Presentation.gif)") 
+    (isExpanded1 ? setExpandedBrightness1("linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 2)), url(/assets/PCEnthusiasts/Presentation.gif)") 
     : 
     setExpandedBrightness1("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(/assets/PCEnthusiasts/Presentation.gif)"));
   };
   const handleChange2 = (panel2) => (event, isExpanded2) => {
     setExpanded2(isExpanded2 ? panel2 : false);
-    (isExpanded2 ? setExpandedBrightness2("linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)), url(/assets/MuseumMapper/MuseumMapperShowcase.gif)")
+    (isExpanded2 ? setExpandedBrightness2("linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 2)), url(/assets/MuseumMapper/MuseumMapperShowcase.gif)")
       :
       setExpandedBrightness2("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(/assets/MuseumMapper/MuseumMapperShowcase.gif)"));
   };
   const handleChange3 = (panel) => (event, isExpanded) => {
     setExpanded3(isExpanded ? panel : false);
-    (isExpanded ? setExpandedBrightness3("linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)), url(/assets/RapidGames/RapidGamesShowcase.gif)")
+    (isExpanded ? setExpandedBrightness3("linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 2)), url(/assets/RapidGames/RapidGamesShowcase.gif)")
       :
       setExpandedBrightness3("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(/assets/RapidGames/RapidGamesShowcase.gif)"));
   };
   const handleChange4 = (panel) => (event, isExpanded) => {
     setExpanded4(isExpanded ? panel : false);
-    (isExpanded ? setExpandedBrightness4("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/assets/BerserkersTrial/Filmage-2021-12-16_130308.gif)")
+    (isExpanded ? setExpandedBrightness4("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1.2)), url(/assets/BerserkersTrial/Filmage-2021-12-16_130308.gif)")
       :
       setExpandedBrightness4("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(/assets/BerserkersTrial/Filmage-2021-12-16_130308.gif)"));
   };
@@ -44,7 +44,7 @@ const ProjectsSection = () => {
 
       <Accordion className="projectAccordion" sx={{ minHeight: "20rem", transition: ".3s", backgroundImage: expandedBrightness1, backgroundSize: "cover", backgroundPosition: "center", marginBottom: "1rem" }} expanded={expanded1 === 'panel1'} onChange={handleChange1('panel1')}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon color="secondary" sx={{ fontSize: "50px", backgroundColor: "#68dcfc", borderRadius: "4px" }} />}
+          expandIcon={<ExpandMoreIcon color="secondary" sx={{ fontSize: "50px", backgroundImage: "linear-gradient(to right top, #092145, #05316c, #0c4094, #234ebe, #415ce8)", borderRadius: "4px" }} />}
           sx={{ backgroundColor: "secondary" }}
         >
           <Typography sx={{ fontWeight:"bold" }} color='secondary' variant="h5" >Project 4: PC Enthusiasts</Typography>
@@ -56,6 +56,11 @@ const ProjectsSection = () => {
             I created a website with React as my front-end and Django as my back-end.
             The website allows you to create different computer builds from a range of components if you are logged in.
           </Typography>
+          <br />
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button href="https://pc-enthusiasts.herokuapp.com" target="_blank" sx={{ fontWeight: "bold", fontSize: "20px" }} variant="contained">Visit page</Button>
+            <Button href="https://github.com/Olys6/PC-Enthusiasts/tree/main#readme" target="_blank" sx={{ fontWeight: "bold", fontSize: "20px" }} variant="contained">look at ReadMe</Button>
+          </div>
         </AccordionDetails>
       </Accordion>
 
@@ -67,13 +72,18 @@ const ProjectsSection = () => {
           <Typography sx={{ fontWeight: "bold" }} color='secondary' variant="h5" >Project 3: Museum Mapper</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{ fontWeight: "bold", textDecoration: "underline" }} variant="h6">1 week / Group of 4</Typography>
+          <Typography sx={{ fontWeight: "bold", textDecoration: "underline", marginTop: "-15px", marginBottom: "-16px"}} variant="h6">1 week / Group of 4</Typography>
           <br />
           <Typography sx={{ fontWeight: "bold" }} variant="h6">
             I made a website for finding museums in England using React and MongoDB in a group of 4.
             My group and I pair coded the back-end and we later worked separately on the front-end.
             This project made me learn a lot about CSS and HTML as I focused on styling.
           </Typography>
+          <br />
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button href="https://museum-mapper.herokuapp.com/" target="_blank" sx={{ fontWeight: "bold", fontSize: "20px" }} variant="contained">Visit page</Button>
+            <Button href="https://github.com/Olys6/SEI-Project-3#readme" target="_blank" sx={{ fontWeight: "bold", fontSize: "20px" }} variant="contained">look at ReadMe</Button>
+          </div>
         </AccordionDetails>
       </Accordion>
 
@@ -93,6 +103,11 @@ const ProjectsSection = () => {
             In this project,
             I learnt a lot about JS fundamentals and logic.
           </Typography>
+          <br />
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button href="https://rapidgames.netlify.app" target="_blank" sx={{ fontWeight: "bold", fontSize: "20px" }} variant="contained">Visit page</Button>
+            <Button href="https://github.com/Olys6/Rapid-Games#readme" target="_blank" sx={{ fontWeight: "bold", fontSize: "20px" }} variant="contained">look at ReadMe</Button>
+          </div>
         </AccordionDetails>
       </Accordion>
 
@@ -112,6 +127,11 @@ const ProjectsSection = () => {
             I worked on a classic grid-based game with HTML, JavaScript and CSS. I chose Pac-man,
             learnt and got a better understanding of JavaScript and CSS from this experience.
           </Typography>
+          <br />
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Button href="https://olys6.github.io/Berserkers-Trial/" target="_blank" sx={{ fontWeight: "bold", fontSize: "20px" }} variant="contained">Visit page</Button>
+            <Button href="https://github.com/Olys6/SEI-Project1#readme" target="_blank" sx={{ fontWeight: "bold", fontSize: "20px" }} variant="contained">look at ReadMe</Button>
+          </div>
         </AccordionDetails>
       </Accordion>
     </section>
